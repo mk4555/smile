2ccae5a9b1be927cba1d2e38d55d0b3e2a165d62
Min Kim <minkimcodes@gmail.com>
2021-02-12 20:27:57 -0500

    live data used for all categories

--- src/pages/Kids.js
+++ src/pages/Kids.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Market } from "../components/Market";
+import { useSelector } from "react-redux";
 
 export const Kids = () => {
+  const items = useSelector((state) => state.items);
+
   return (
     <div>
-      <Market />
+      <Market items={items} />
     </div>
   );
 };
